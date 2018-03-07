a64a60cb0c779d2f44f042ea5f40438082f19c57
unknown <tallen904@gmail.com>
2018-03-06 20:49:41 -0800

    Updated html-routes.js and set up employee-api-routes.js

--- routes/employee-api-routes.js
+++ routes/employee-api-routes.js
@@ -1,3 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
+
+// Get all employee information
+router.get("/api/employees", (req, res) => {
+  db.Employee.findAll({ include: [db.EmployeeSkills] }).then(employees => {
+    res.json(employees);
+  });
+});
+
+// Post a new employee
+router.post("/api/employees", (req, res) => {
+  db.Employee.create(req.body,
+    {
+      include: [db.EmployeeSkills]
+    }
+  ).then((dbpost) => res.json(dbpost))
+});
+
+module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,4 +6,24 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/index.html'))
 })
 
+router.get('/employee', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/employee.html'))
+})
+
+router.get('/employer', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/employer.html'))
+})
+
+router.get('/postjob', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/jobposting.html'))
+})
+
+router.get('/jobs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/alljobs.html'))
+})
+
+router.get('/match', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/match.html'))
+})
+
 module.exports = router;
