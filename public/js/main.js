1f51b4fbade4cda35ea3d61851b4a7b0b1ee891c
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-03-07 12:10:14 -0800

    file name changes

--- public/js/main.js
+++ public/js/main.js
@@ -10,36 +10,8 @@ $(document).ready(function () {
 
 
     $(document).on('click', '.post-btn', function(){
-       renderPostPage()
+       //Link to employer page (employer.html)
     })
 
 });
 
-function renderPostPage() {
-    $('.all-content').empty();
-    $('body').css('background-image', 'none');
-    insertNavBar();
-    const formHolder = $('<div class="form-holder">')
-    $('.all-content').append(formHolder)
-
-    
-}
-
-function insertNavBar() {
-    const navBar = 
-    `<nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <div class="header-text">
-                    <h class="tech-2">TECH</h>
-                    <h class="mate-2">MATE</h>
-                </div>
-                <div class="header-links">
-                    <a href="#">Home</a>
-                    <a href="#">Show Your Skills</a>
-                </div>
-            </div>
-        </div>
-    </nav>`
-    $('.all-content').append(navBar);
-}
